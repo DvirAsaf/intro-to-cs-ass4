be800dba1c8fab80c5de772956b146bdf4a71ebf
DvirAsaf <59093573+DvirAsaf@users.noreply.github.com>
2019-12-22 22:31:08 +0200

    הגעתי לשינוי הלוח :)

--- ass4/ass4.c
+++ ass4/ass4.c
@@ -822,55 +822,60 @@ int checkValidPromotion(char board[][SIZE],Move move) {
     }
 }
 
-int isValidMove(char board[][SIZE], Move move){
-    //TODO: לבדוק שלא מתנגשים בקיר או עוברים אותו :)
-
-    if(move.player == PAWN){
-        if(checkValidPawn(board,move) == 0)
-            return  0;
-    }
-    else if(move.player == ROOK)
-    {
-        if(checkValidRook(board,move) == 0)
-            return 0;
-    }
-    else if(move.player == KNIGHT)
-    {
-        if(checkValidKnight(board,move) == 0)
-            return 0;
-    }
-    else if(move.player == BISHOP)
-    {
-        if(checkValidBishop(board,move)==0)
-            return 0;
-    }
-    else if(move.player == QUEEN)
-    {
-        if(checkValidQueen(board,move)==0)
-            return 0;
-    }
-    else if(move.player == KING)
-    {
-        if(checkValidKing(board,move)==0)
-            return 0;
-    }
-//    return 1;
-}
-
-int searchSrcRow(board[][SIZE],Move move)
+int isValidMove(char board[][SIZE], Move move)
 {
-    if(move.hasSrcCol)
-    {
-
+    switch (move.player) {
+        case 'P':
+        {
+            if(!checkValidPawn(board,move))
+                return  0;
+            break;
+        }
+        case 'R':
+        {
+            if(checkValidRook(board,move) == 0)
+                return 0;
+            break;
+        }
+        case 'Q':
+        {
+            if(checkValidQueen(board,move)==0)
+                return 0;
+            break;
+        }
+        case 'K':
+        {
+            if(checkValidKing(board,move)==0)
+                return 0;
+            break;
+        }
+        case 'B':
+        {
+            if(checkValidBishop(board,move)==0)
+                return 0;
+            break;
+        }
+        case 'N':
+        {
+            if(checkValidKnight(board,move) == 0)
+                return 0;
+            break;
+        }
     }
+    if(checkValidPromotion(board,move) == 0)
+        return 0;
+
+    if(!checkValidCapture(board,move))
+        return 0;
+
+    //TODO: Add check for shach and mat
+
+
+
+    return 1;
 }
 
-int searchSrcCol(board[][SIZE],Move move)
-{
-
-}
-
-Location SearchBlackPawn(board[][SIZE],Move move)
+Location searchBlackPawn(char board[][SIZE],Move move)
 {
     Location loc;
     if(move.isCapture)
@@ -897,309 +902,375 @@ Location SearchBlackPawn(board[][SIZE],Move move)
     return loc;
 }
 
-Location searchSrcBlack(board[][SIZE],Move move)
+Location searchWhitePawn(char board[][SIZE],Move move)
+{
+    Location loc;
+    if(move.isCapture)
+    {
+        loc.row = move.iDest+1;
+        if(board[loc.row][move.jDest-1] == WHITE_PAWN)
+            //left col
+            loc.col = move.jDest - 1 ;
+        else
+            //right col
+            loc.col = move.jDest + 1;
+
+    } else{
+        loc.col = move.jDest;
+        //can be double step
+        if(move.iDest == SIZE - 4)
+        {
+            if(board[move.iDest+1][move.jDest] == WHITE_PAWN)
+                loc.row = move.iDest + 1;
+            else
+                loc.row = move.iDest + 2;
+        } else{
+            loc.row = move.iDest + 1;
+        }
+    }
+    return loc;
+}
+
+Location searchRook(char board[][SIZE],Move move,char player)
+{
+    Location loc;
+    int flag = 0;
+    for (int j = 0; j < SIZE; ++j)
+    {
+        if(board[move.iDest][j]==player)
+        {
+            loc.col = j;
+            loc.row = move.iDest;
+            flag=1;
+            break;
+        }
+        if(board[move.iDest][j]!=EMPTY)
+            break;
+    }
+    if(!flag)
+    {
+        for (int i = 0; i < SIZE; ++i)
+        {
+            if(board[i][move.jDest]==player)
+            {
+                loc.row = i;
+                loc.col = move.jDest;
+                break;
+            }
+            if(board[i][move.jDest]!=EMPTY)
+                break;
+        }
+    }
+    return loc;
+}
+
+Location searchKing(char board[][SIZE],Move move)
+{
+    char playerColor;
+    if(move.isWhite)
+        playerColor = WHITE_KING;
+    else
+        playerColor = BLACK_KING;
+    Location loc;
+    for (int i = move.iDest-1; i <=move.iDest+1 ; ++i)
+    {
+        for (int j = move.jDest-1; j <= move.jDest+1 ; ++j)
+        {
+            if(i<0 || i>=SIZE || j<0 || j>=SIZE)
+                continue;
+            if(board[i][j]==playerColor)
+            {
+                loc.col = j;
+                loc.row = i;
+                break;
+            }
+        }
+    }
+    return loc;
+}
+
+Location searchBishop(char board[][SIZE],Move move,char player)
+{
+    Location loc;
+    loc.row = -1;
+    loc.col = -1;
+    int flag = 0;
+    int j = move.jDest-1;
+    //diagnol line from left corner to right down
+    for (int i = move.iDest - 1; i >=0 ; i--)
+    {
+        if(j<0)
+            break;
+        if(board[i][j] == player)
+        {
+            loc.col = j;
+            loc.row = i;
+            flag = 1;
+        }
+        if(board[i][j] != EMPTY)
+            break;
+        j--;
+    }
+    if(!flag)
+    {
+        j = move.jDest+1;
+        //diagnol line up and left
+        for (int i = move.iDest + 1; i <SIZE ; i++)
+        {
+            if(j>=SIZE)
+                break;
+            if(board[i][j] == player)
+            {
+                loc.col = j;
+                loc.row = i;
+                flag = 1;
+            }
+            if(board[i][j] != EMPTY)
+                break;
+            j++;
+        }
+    }
+    if(!flag)
+    {
+        j = move.jDest-1;
+        //diagnol line up and right
+        for (int i = move.iDest + 1; i <SIZE ; i++)
+        {
+            if(j<0)
+                break;
+            if(board[i][j] == player)
+            {
+                loc.col = j;
+                loc.row = i;
+                flag = 1;
+            }
+            if(board[i][j] != EMPTY)
+                break;
+            j--;
+        }
+    }
+    if(!flag)
+    {
+        j = move.jDest+1;
+        //diagnol line down and left
+        for (int i = move.iDest - 1; i >= 0 ; i--)
+        {
+            if(j>=SIZE)
+                break;
+            if(board[i][j] == player)
+            {
+                loc.col = j;
+                loc.row = i;
+            }
+            if(board[i][j] != EMPTY)
+                break;
+            j++;
+        }
+    }
+    return loc;
+}
+
+Location searchKnight(char board[][SIZE],Move move)
+{
+    char playerColor;
+    if(move.isWhite)
+        playerColor = WHITE_KNIGHT;
+    else
+        playerColor = BLACK_KNIGHT;
+    Location loc;
+    int flag = 0;
+    int i = move.iDest,j=move.jDest;
+
+    //2 steps row one col
+    if(i-2>=0 && j-1>=0)
+    {
+        if(board[i-2][j-1] == playerColor)
+        {
+            loc.row = i-2;
+            loc.col = j-1;
+            flag=1;
+        }
+    }
+    if(!flag)
+    {
+        if(i-2>=0 && j+1<SIZE)
+        {
+            if(board[i-2][j+1] == playerColor)
+            {
+                loc.row = i-2;
+                loc.col = j+1;
+                flag=1;
+            }
+        }
+    }
+    if(!flag)
+    {
+        if(i+2<SIZE && j+1<SIZE)
+        {
+            if(board[i+2][j+1] == playerColor)
+            {
+                loc.row = i+2;
+                loc.col = j+1;
+                flag=1;
+            }
+        }
+    }
+    if(!flag)
+    {
+        if(i+2<SIZE && j-1>=0)
+        {
+            if(board[i+2][j-1] == playerColor)
+            {
+                loc.row = i+2;
+                loc.col = j-1;
+                flag=1;
+            }
+        }
+    }
+
+    //2 steps col one row
+    if(!flag)
+    {
+        if(i+1<SIZE && j-2>=0)
+        {
+            if(board[i+1][j-2] == playerColor)
+            {
+                loc.row = i+1;
+                loc.col = j-2;
+                flag=1;
+            }
+        }
+    }
+
+    if(!flag)
+    {
+        if(i+1<SIZE && j+2< SIZE)
+        {
+            if(board[i+1][j+2] == playerColor)
+            {
+                loc.row = i+1;
+                loc.col = j+2;
+                flag=1;
+            }
+        }
+    }
+
+    if(!flag)
+    {
+        if(i-1>=0 && j-2>=0)
+        {
+            if(board[i-1][j-2] == playerColor)
+            {
+                loc.row = i-1;
+                loc.col = j-2;
+                flag=1;
+            }
+        }
+    }
+
+    if(!flag)
+    {
+        if(i-1>=0 && j+2<SIZE)
+        {
+            if(board[i-1][j+2] == playerColor)
+            {
+                loc.row = i-1;
+                loc.col = j+2;
+            }
+        }
+    }
+    return loc;
+}
+
+Location searchQueen(char board[][SIZE],Move move)
+{
+    char playerColor;
+    if(move.isWhite)
+        playerColor = WHITE_QUEEN;
+    else
+        playerColor = BLACK_QUEEN;
+    Location loc;
+    loc = searchBishop(board,move,playerColor);
+    //we didnt find this in bishop path, so let try to find it in rook path
+    if(loc.row==-1 || loc.col==-1)
+    {
+        //search the same whay so use this function
+        loc = searchRook(board,move,playerColor);
+    }
+    return loc;
+}
+
+Location searchSrc(char board[][SIZE],Move move)
 {
     //TODO: לבדוק מה תשובת המרצה ואם לא חוקי אז תמיד לבדוק שהתו הנבדק אם הוא לא החייל המבוקש שהוא תו ריק לעשות ברייק  ולהמשיך לחפש בכיוונים השונים
     Location loc;
     switch (move.player) {
-        case PAWN:
+        case 'P':
         {
-            if(move.isCapture)
-            {
-                loc.row = move.iDest-1;
-                if(board[loc.row][move.jDest-1] == BLACK_PAWN)
-                    loc.col = move.jDest - 1 ;
-                else
-                    loc.col = move.jDest + 1;
-
-            } else{
-                loc.col = move.jDest;
-                //can be double step
-                if(move.iDest == 3)
-                {
-                    if(board[move.iDest-1][move.jDest] == BLACK_PAWN)
-                        loc.row = move.iDest - 1;
-                    else
-                        loc.row = move.iDest - 2;
-                } else{
-                    loc.row = move.iDest - 1;
-                }
-            }
+            if(move.isWhite)
+                loc = searchWhitePawn(board,move);
+            else
+                loc = searchBlackPawn(board,move);
             break;
         }
-        case ROOK:
+        case 'R':
         {
-            int flag = 0;
-            for (int j = 0; j < SIZE; ++j)
-            {
-                if(board[move.iDest][j]==BLACK_ROOK)
-                {
-                    loc.col = j;
-                    loc.row = move.iDest;
-                    flag=1;
-                    break;
-                }
-            }
-            if(!flag)
-            {
-                for (int i = 0; i < SIZE; ++i)
-                {
-                    if(board[i][move.jDest]==BLACK_ROOK)
-                    {
-                        loc.row = i;
-                        loc.col = move.jDest;
-                        break;
-                    }
-                }
-            }
+            char playerColor;
+            if(move.isWhite)
+                playerColor = WHITE_ROOK;
+            else
+                playerColor = BLACK_ROOK;
+
+            loc = searchRook(board,move,playerColor);
             break;
         }
-        case QUEEN:
+        case 'Q':
         {
-
+            loc = searchQueen(board,move);
             break;
         }
-        case KING:
+        case 'K':
         {
-//            if(move.iDest==0||move.)
-            for (int i = move.iDest-1; i <=move.iDest+1 ; ++i)
-            {
-                for (int j = move.jDest-1; j <= move.jDest+1 ; ++j)
-                {
-                    if(i<0 || i>=SIZE || j<0 || j>=SIZE)
-                        continue;
-                    if(board[i][j]==BLACK_KING)
-                        {
-                        loc.col = j;
-                        loc.row = i;
-                            break;
-                        }
-                }
-            }
+            loc = searchKing(board,move);
             break;
         }
-        case BISHOP:
+        case 'B':
         {
-            int flag = 0;
-            int j = move.jDest-1;
-            //diagnol line from left corner to right down
-            for (int i = move.iDest - 1; i >=0 ; i--)
-            {
-                if(j<0)
-                    break;
-                if(board[i][j] == BLACK_BISHOP)
-                {
-                    loc.col = j;
-                    loc.row = i;
-                    flag = 1;
-                }
-                j--;
-            }
-            if(!flag)
-            {
-                j = move.jDest+1;
-                //diagnol line up and left
-                for (int i = move.iDest + 1; i <SIZE ; i++)
-                {
-                    if(j>=SIZE)
-                        break;
-                    if(board[i][j] == BLACK_BISHOP)
-                    {
-                        loc.col = j;
-                        loc.row = i;
-                        flag = 1;
-                    }
-                    j++;
-                }
-            }
-            if(!flag)
-            {
-                j = move.jDest-1;
-                //diagnol line up and right
-                for (int i = move.iDest + 1; i <SIZE ; i++)
-                {
-                    if(j<0)
-                        break;
-                    if(board[i][j] == BLACK_BISHOP)
-                    {
-                        loc.col = j;
-                        loc.row = i;
-                        flag = 1;
-                    }
-                    j--;
-                }
-            }
-            if(!flag)
-            {
-                j = move.jDest+1;
-                //diagnol line down and left
-                for (int i = move.iDest - 1; i >= 0 ; i--)
-                {
-                    if(j>=SIZE)
-                        break;
-                    if(board[i][j] == BLACK_BISHOP)
-                    {
-                        loc.col = j;
-                        loc.row = i;
-                    }
-                    j++;
-                }
-            }
+            char playerColor;
+            if(move.isWhite)
+                playerColor = WHITE_BISHOP;
+            else
+                playerColor = BLACK_BISHOP;
+            loc = searchBishop(board,move,playerColor);
             break;
         }
-        case KNIGHT:
+        case 'N':
         {
-            int flag = 0;
-            int i = move.iDest,j=move.jDest;
-
-            //2 steps row one col
-            if(i-2>=0 && j-1>=0)
-            {
-                if(board[i-2][j-1] == BLACK_KNIGHT)
-                {
-                    loc.row = i-2;
-                    loc.col = j-1;
-                    flag=1;
-                }
-            }
-            if(!flag)
-            {
-                if(i-2>=0 && j+1<SIZE)
-                {
-                    if(board[i-2][j+1] == BLACK_KNIGHT)
-                    {
-                        loc.row = i-2;
-                        loc.col = j+1;
-                        flag=1;
-                    }
-                }
-            }
-            if(!flag)
-            {
-                if(i+2<SIZE && j+1<SIZE)
-                {
-                    if(board[i+2][j+1] == BLACK_KNIGHT)
-                    {
-                        loc.row = i+2;
-                        loc.col = j+1;
-                        flag=1;
-                    }
-                }
-            }
-            if(!flag)
-            {
-                if(i+2<SIZE && j-1>=0)
-                {
-                    if(board[i+2][j-1] == BLACK_KNIGHT)
-                    {
-                        loc.row = i+2;
-                        loc.col = j-1;
-                        flag=1;
-                    }
-                }
-            }
-
-            //2 steps col one row
-            if(!flag)
-            {
-                if(i+1<SIZE && j-2>=0)
-                {
-                    if(board[i+1][j-2] == BLACK_KNIGHT)
-                    {
-                        loc.row = i+1;
-                        loc.col = j-2;
-                        flag=1;
-                    }
-                }
-            }
-
-            if(!flag)
-            {
-                if(i+1<SIZE && j+2< SIZE)
-                {
-                    if(board[i+1][j+2] == BLACK_KNIGHT)
-                    {
-                        loc.row = i+1;
-                        loc.col = j+2;
-                        flag=1;
-                    }
-                }
-            }
-
-            if(!flag)
-            {
-                if(i-1>=0 && j-2>=0)
-                {
-                    if(board[i-1][j-2] == BLACK_KNIGHT)
-                    {
-                        loc.row = i-1;
-                        loc.col = j-2;
-                        flag=1;
-                    }
-                }
-            }
-
-            if(!flag)
-            {
-                if(i-1>=0 && j+2<SIZE)
-                {
-                    if(board[i-1][j+2] == BLACK_KNIGHT)
-                    {
-                        loc.row = i-1;
-                        loc.col = j+2;
-                    }
-                }
-            }
+            loc = searchKnight(board,move);
             break;
         }
     }
     return loc;
 }
 
-Location searchSrcWhite(board[][SIZE],Move move)
+void changeBoard(char board[][SIZE])
 {
-    Location loc;
-    switch (move.player) {
-        case PAWN:
-        {
-            if(move.isCapture)
-            {
-
-            } else{
-                loc.col = move.jDest;
-
-            }
-            break;
-        }
-        case ROOK:
-            break;
-        case QUEEN:
-            break;
-        case KING:
-            break;
-        case BISHOP:
-            break;
-        case KNIGHT:
-            break;
-    }
-    if(move.player == PAWN)
-    {
-        if(move.isCapture)
-        {
-
-        } else{
-            loc.col = move.jDest;
-
-        }
-
-
-    }
-
-
-    return loc;
+    board[0][0] = 'c';
 }
 
+char capitalToLower(char capitalLetter)
+void updateBoard(char board[][SIZE],Move move)
+{
+    //clean src location
+    board[move.iSrc][move.jSrc] = EMPTY;
 
+    //update dest location
+    char player;
+
+    if(move.isWhite)
+        board[move.iDest,move.jDest] = tolower(move.player);
+
+}
 
 int makeMove(char board[][SIZE], char pgn[], int isWhiteTurn)
 {
@@ -1223,7 +1294,6 @@ int makeMove(char board[][SIZE], char pgn[], int isWhiteTurn)
         move.destCol = findDstCol(pgn,1);
         move.hasSrcCol = 1;
 
-
     } else{
         move.destCol = findDstCol(pgn,0);
     }
@@ -1241,15 +1311,28 @@ int makeMove(char board[][SIZE], char pgn[], int isWhiteTurn)
 
     if(!move.hasSrcRow || !move.hasSrcCol)
     {
-        //SET_SRC+
+        Location loc = searchSrc(board,move);
+        if(move.hasSrcRow) {
+            move.iSrc = changeCharToIndex(move.srcRow);
+            move.jSrc = loc.col;
+        }else if(move.hasSrcCol)
+        {
+            move.jSrc = changeCharToIndex(move.srcCol);
+            move.iSrc = loc.row;
+        } else{
+            move.iSrc = loc.row;
+            move.jSrc = loc.col;
+        }
+    }
+    else{
+        //TODO: אחרי שמוצאים את נקודת המוצא אפשר לעדכן את האינדקסים של המוצא
+        //update index values:
+        move.jSrc = changeCharToIndex(move.srcCol);
+        move.iSrc = changeCharToIndex(move.srcRow);
     }
 
-    //TODO: אחרי שמוצאים את נקודת המוצא אפשר לעדכן את האינדקסים של המוצא
-    //update index values:
-    move.jSrc = changeCharToIndex(move.srcCol);
-    move.iSrc = changeCharToIndex(move.srcRow);
-
-
+    if(!isValidMove(board,move))
+        return 0;
 
 
 }
